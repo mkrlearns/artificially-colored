9ed581177d755a6dbdbf9defbf95eee20b8bf4aa
ryan m <contact@ryanmeek.com>
2019-12-31 15:03:59 +0000

    initial API connection

--- lib/artificially_colored/ai.rb
+++ lib/artificially_colored/ai.rb
@@ -1,3 +1,27 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class ArtificiallyColored::AI
+
+  def ai_connect
+    test = [[44,43,44],[90,83,82],"N","N","N"]
+    
+    uri = URI.parse("http://colormind.io/api/")
+    request = Net::HTTP::Post.new(uri)
+    request.body = JSON.dump({ "model" => "default", "input" => test })
+    
+    req_options = { use_ssl: uri.scheme == "https" }
+    
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    
+    if response.code == '200'
+      puts response.body
+    else
+      puts 'Unable to connect to API.'
+    end
+  end
 
 end
