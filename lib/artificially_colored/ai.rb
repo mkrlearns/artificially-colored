5f92f7961312389fa302510bf8786a24d8797d8b
ryan m <contact@ryanmeek.com>
2019-12-31 17:54:51 +0000

    scraper add elements

--- lib/artificially_colored/ai.rb
+++ lib/artificially_colored/ai.rb
@@ -1,11 +1,16 @@
 class ArtificiallyColored::AI
 
-  def ai_connect
-    test = [[44,43,44],[90,83,82],"N","N","N"]
+  def ai_connect(array)
+
+    if array.include(',') then array = array.split(',')
+    else array = [array.strip] end
+    
+    # array.map! { |color| convert_color...
+    
     
     uri = URI.parse("http://colormind.io/api/")
     request = Net::HTTP::Post.new(uri)
-    request.body = JSON.dump({ "model" => "default", "input" => test })
+    request.body = JSON.dump({ "model" => "default", "input" => array })
     
     req_options = { use_ssl: uri.scheme == "https" }
     
@@ -21,3 +26,5 @@ class ArtificiallyColored::AI
   end
 
 end
+
+#test = [[44,43,44],[90,83,82],"N","N","N"]
--- lib/artificially_colored/scraper.rb
+++ lib/artificially_colored/scraper.rb
@@ -1,3 +1,38 @@
 class ArtificiallyColored::Scraper
 
+  attr_accessor :hex, :rgb, :hsl, :hsv
+  
+  def initialize(hex=nil, rgb=nil, hsl=nil, hsv=nil)
+    
+  end
+
+  def convert_color(color)
+    color = color.delete!('()°%').gsub!(',', '_').gsub!(' ', '')
+    
+    if color.downcase.include? 'rgb'
+      url = color.sub!(/rgb/i, 'rgb-color')
+    elsif color.include? '#'
+      url = color.sub!('#', 'hex-color-')
+    elsif color.downcase.include? 'hsl'
+      url = color.sub!(/hsl/i, 'hsl-color')
+    elsif color.downcase.include? 'hsv'
+      url = color.sub!(/hsv/i, 'hsv-color')
+    elsif color.length == 3 && !color.match(/\A[a-zA-Z0-9]*\z/).nil?
+      url = "hex-color-#{color}#{color}"
+    elsif color.length == 6 && !color.match(/\A[a-zA-Z0-9]*\z/).nil?
+      url = "hex-color-#{color}"
+    else
+      puts "Invalid color code, try again."
+  
+  doc = Nokogiri::HTML(open("https://convertingcolors.com/#{url.strip}.html"))
+  
+  rgb = doc.css('#copyRgbtext').text
+  hex = doc.css('#copyHextext').text
+  hsl = doc.css('#copyHsltext').text
+  hsv = doc.css('#copyHsvtext').text
+  
+  return { rgb: "rgb(#{rgb})", hex: "##{hex}", hsl: "hsl(#{hsl})", hsv: "rgb(#{hsv})"}
+  
+  end
+
 end
