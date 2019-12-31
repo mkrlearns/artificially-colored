d722e7e1246b130ea87145588725dafe3da1138b
ryan m <contact@ryanmeek.com>
2019-12-31 23:50:01 +0000

    build menu for ai

--- lib/artificially_colored/cli.rb
+++ lib/artificially_colored/cli.rb
@@ -5,29 +5,49 @@ class ArtificiallyColored::CLI
   end
   
   def main_menu
-    colors = ['#AE81FF', "#66D9EF", "#A6E22E", "#E6DB74", "#E69F66", "#FD971F", "#F92672"]
-    loading_color = Rainbow("▎").color(colors.sample)
+    ai_menu
+  end
+
+  def ai_menu
+    puts "Enter a comma seperated list of 1-4 colors."
+    puts "A five color AI generated palette will be returned."
+    user_colors = gets
+    clear
+    puts "How many palettes would you like to generate?"
+    puts "The higher the number the longer it will take,"
+    puts "with ten being the highest you can return."
+    puts "Any duplicates generated will be removed from results."
+    user_num = gets.strip.to_i
+    user_num = 10 if user_num > 10
+    ai_results(user_colors, user_num)
+  end
+
+  def ai_results(user_colors, user_num)
     palettes = []
-    5.times do
+    all = []
+    loading = fake_loader
+    user_num.times do
       clear
-      puts "LOADING: #{loading_color}"
-      6.times { loading_color += Rainbow("▎").color(colors.sample) }
+      puts loading + "\e[?25l"
+      loading += fake_loader + fake_loader
       swatches = []
-      ArtificiallyColored::AI.new.ai_connect('D27042').each do |i|
+      ArtificiallyColored::AI.new.ai_connect(user_colors).each do |i|
         swatches << "#{Rainbow("▇" * 6).color(i[:hex])}"
+        all << i
       end
       palettes << "#{swatches[0]} #{swatches[1]} #{swatches[2]} #{swatches[3]} #{swatches[4]}"
     end
     clear
-    i = 0
-    palettes.uniq.each do |palette|
-      i += 1
-      puts "#{i}: #{palette}\n"
-    end
+    palettes.uniq.each { |palette| puts "#{palettes.uniq.index(palette) + 1}: #{palette}\n" }
+    puts "\e[?25h"
   end
 
   def clear
     system "clear" or system "cls"
+  end
+
+  def fake_loader
+    Rainbow("▎" * 2).color(['#AE81FF',"#66D9EF","#E69F66","#FD971F","#F92672"].sample) 
   end
 
 end
